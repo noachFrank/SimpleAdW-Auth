5d438f258b5eab69b90f3396ba366664cd05aaf6
noachFrank <139394778+noachFrank@users.noreply.github.com>
2023-07-28 14:57:37 -0400

    Update DbManager.cs

--- SimpleAd_withAuth.data/DbManager.cs
+++ SimpleAd_withAuth.data/DbManager.cs
@@ -43,38 +43,6 @@ namespace SimpleAd_withAuth.data
 
         }
 
-        public List<Ad> GetAllAdsForMyAccount(int id)
-        {
-            using SqlConnection connection = new SqlConnection(_connectionString);
-            using SqlCommand command = connection.CreateCommand();
-            command.CommandText = @"SELECT * FROM Ad a
-                            JOIN Lister l
-                            ON l.Id = a.ListerId
-                            WHERE ListerId = @id
-                            ORDER BY DATE DESC";
-            command.Parameters.AddWithValue("@Id", id);
-            connection.Open();
-            var ads = new List<Ad>();
-            var reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                ads.Add(new Ad
-                {
-                    Id = (int)reader["Id"],
-                    ListerId = (int)reader["ListerId"],
-                    Name = (string)reader["Name"],
-                    ListingDate = (DateTime)reader["Date"],
-                    PhoneNumber = (string)reader["PhoneNumber"],
-                    Listing = (string)reader["Listing"]
-
-                });
-            }
-
-
-            return ads;
-
-        }
-
         public void NewLister(Lister lister, string password)
         {
             using SqlConnection connection = new SqlConnection(_connectionString);
